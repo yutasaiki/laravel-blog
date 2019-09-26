039ae8734e06205fb0d48dbedd123dfb54a6fe6c
saiki-y <yuta.saiki.w1@gmail.com>
2019-09-26 22:44:44 +0900

    ユーザーデータを汎用的なものに修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,27 +2,19 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run()
     {
-        $user_name = 'y_saiki';
-
         // ユーザー登録はこのSeederを通して行う。
         DB::table('users')->insert([
-            // 'name' => Str::random(10),
-            // 'email' => Str::random(10).'@gmail.com',
-            // 'password' => bcrypt('password'),
-            'name' => $user_name,
-            'email' => 'yuta.saiki.w1@gmail.com',
-            'password' => bcrypt('password'),
+            'name' => 'laravel_blog',
+            'email' => 'laravel_blog@gmail.com',
+            'password' => bcrypt('laravel_blog'),
         ]);
     }
 }
