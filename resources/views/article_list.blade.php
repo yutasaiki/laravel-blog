10553ff7fca4e34f3bf08520e8dc7676e324cca2
saiki-y <yuta.saiki.w1@gmail.com>
2019-10-03 20:33:10 +0900

    作業中コミット。一旦、デザインの取り込みは完了。

--- resources/views/article_list.blade.php
+++ resources/views/article_list.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-        {{-- 文書構造も宮本さんのデザイン修正が終わったら実装する。それまではデータを表示させるために仮で作成 --}}
-        <h1>ここは記事一覧画面です。</h1>
         <a href="{{ url('/blog/article/create') }}"><button type="button">記事作成</button></a>
-        <hr>
-        @foreach($articles as $article)
-        タイトル ： {{$article->title}}
-        <br>
-        本文 ： {{$article->body}}
-        <br>
-        <a href="{{ url('/blog/article/detail?article_id='.$article->id) }}"><button type="button">詳細</button></a>
-        <a href="{{ url('/blog/article/edit?article_id='.$article->id) }}"><button type="button">編集</button></a>
-        <hr>
-        @endforeach
+        <div class="background">
+                @foreach($articles as $article)
+                        <div class="card">
+                                        <div class="card-container">
+                                                <div class="title-box">
+                                                        <p class="title">{{$article->title}}</p>
+                                                        <a href="{{ url('/blog/article/detail?article_id='.$article->id) }}"><button type="button">詳細</button></a>
+                                                        <a href="{{ url('/blog/article/edit?article_id='.$article->id) }}"><button type="button">編集</button></a>
+                                                </div>
+                                                <div class="line"></div>
+                                                <div class="desc-box">
+                                                        <p class="desc">{{$article->body}}</p>
+                                                </div>
+                                        </div>
+                                </div>        
+                @endforeach
+        </div>
 @endsection
