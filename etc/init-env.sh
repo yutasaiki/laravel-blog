bfad775c73fb204f1beb61430f922c406ce2f738
miyamoto <miyamotok0105@gmail.com>
2019-10-30 07:23:10 +0900

    [Fix] パスとかミス

--- etc/init-env.sh
+++ etc/init-env.sh
@@ -4,15 +4,16 @@
 
 composer install
 
+touch database/database.sqlite
 abspath="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
-
 sqlitePath=DB_DATABASE=${abspath}database/database.sqlite
 
-echo ""${sqlitePath} >> .env
+cp .env.example .env
+echo ${sqlitePath} >> .env
 
 php artisan migrate
 php artisan db:seed
 php artisan key:generate
 
-
+php artisan serve
 
