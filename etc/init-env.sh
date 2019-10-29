d1932ef2688be49739c7af7f235b35c71b07ea91
miyamoto <miyamotok0105@gmail.com>
2019-10-30 07:09:01 +0900

    [Add] 初期設定

---
+++ etc/init-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Macで動作
+
+composer install
+
+abspath="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
+
+sqlitePath=DB_DATABASE=${abspath}database/database.sqlite
+
+echo "\n"${sqlitePath} >> .env
+
+php artisan migrate
+php artisan db:seed
+php artisan key:generate
+
+
+
